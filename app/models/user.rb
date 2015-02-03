8216a4f5cfb3da383b7e4532bb97deddd8eb18b3
Martin Granados <margrangr@gmail.com>
2015-02-03 13:47:14 -0600

    Model users w/authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :assignments
   has_many :trials, through: :assigments
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130005122) do
+ActiveRecord::Schema.define(version: 20150203193626) do
 
   create_table "assignments", force: true do |t|
     t.integer  "trial_id"
@@ -103,10 +103,10 @@ ActiveRecord::Schema.define(version: 20150130005122) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "username"
-    t.string   "password"
     t.string   "privileges"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
