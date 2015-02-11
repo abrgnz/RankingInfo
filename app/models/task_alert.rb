f506cb67ab7342ba2e1066b4332eb35142fe8f68
Abraham González <gnz.abr@gmail.com>
2015-02-11 17:23:45 -0600

    Version 0.1.1

--- app/models/task_alert.rb
+++
@@ -1,4 +0,0 @@
-class TaskAlert < ActiveRecord::Base
-  belongs_to
-  belongs_to
-end
--- app/models/trial_alert.rb
+++
@@ -1,6 +0,0 @@
-class TrialAlert < ActiveRecord::Base
-  belongs_to :trial
-  def self.notifications(session)
-    TrialAlert.where('user_id = ?',session)
-  end
-end
